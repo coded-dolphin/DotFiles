9b0434ff793b7106fa71004c72418a7ffc6a55c2
coded-dolphin <nepalsarbagya69@gmail.com>
2023-04-28 09:08:54 +0545

    nvim config and adding wallpapers

--- .config/bspwm/scripts/autostart.sh
+++ .config/bspwm/scripts/autostart.sh
@@ -3,6 +3,7 @@
 sxhkd -c ~/.config/bspwm/scripts/sxhkdrc &
 ./home/chain/.local/bin/batterynotify &
 ~/.config/polybar/launch.sh &
+xsetroot -cursor_name left_ptr &
 
 nitrogen --restore &
 xfce4-power-manager &
